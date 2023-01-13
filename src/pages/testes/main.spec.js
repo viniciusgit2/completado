993ca05f792c3fa215ff8b96463effa8ec5d8d23
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-13 11:26:39 -0300

    quase pronto

--- src/pages/testes/main.spec.js
+++ src/pages/testes/main.spec.js
@@ -1,1 +1,12 @@
-import {expect} from "chai"
+import {expect,} from "chai"
+import { search } from "../spote/main"
+describe("spote",()=>{
+describe("smoke test",()=>{
+it(`should exist the search method`),()=>{
+expect(search).to.exist
+}
+}
+)
+
+}
+)
