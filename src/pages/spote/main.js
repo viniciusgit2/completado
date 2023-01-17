0b249c60f727bae787c716b06ea3ee26d2b02c82
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-17 11:14:28 -0300

    eslint passou o resto da acesso negado

--- src/pages/spote/main.js
+++ src/pages/spote/main.js
@@ -1,10 +1,10 @@
-export const search=()=>
-fetch( `https://open.spotify.com/?`)
-//generico
+export const search=(query,type)=>
+fetch( `https://api.spotify.com/${query}&${type}`)
+
 export const searchalbum=()=>{}
 export const searchartist=()=>
 fetch( `https://api.spotify.com/v1/me`)
 export const searchplaylist=()=>{}
 export const track=()=>
 fetch(`https://api.spotify.com/v1/me`)
-//copie do artist
+//copie do artist(meu test)
--- src/pages/testes/main.spec.js
+++ src/pages/testes/main.spec.js
@@ -10,28 +10,37 @@ global.fetch=require(node-fetch)
 describe("spote",()=>{
 describe("smoke test",()=>{
 it(`should call fetch function`),()=>{
-    const fetchstub=Sinon.stub(global,"fetch")
- 
-    expect(fetchstub).to.have.been.calledOnce
-}
-it(`should receiv correct the url to fetch`),()=>{
+    context(`expect to passing`),()=>{
+        const searchartist=search(`https://api.spotify.com/v1/me`);
+        expect(fetchstub).to.have.exist
+        const fetchstub=Sinon.stub(global,"fetch")
+        expect(fetchstub).to.have.been.calledOnce
+    }
+    context(`expect to passing`),()=>{
    
-    const searchartist=search(`https://api.spotify.com/v1/me.`);
-    expect(fetchstub).to.have.exist
+    it(`should receiv correct the url to fetch`),()=>{
+   
+        const searchartist=search(`https://api.spotify.com/v1/me`);
+        expect(fetchstub).to.have.exist
+   
 }
 it(`should receive correct url to be fetch`),()=>{
 
-  const searchartist=search(`kkk,artist`)
-  expect(fetchstub).to.been.calledWith("https://api.spotify.com/v1/me")
+    const searchartist=search(`kkk,artist`)
+    expect(fetchstub).to.been.calledWith(`https://api.spotify.com/v1/me`)
+    fetchstub.restore(`https://api.spotify.com/v1/me`)
+  }
 }
+
 it(`should exist the search method`),()=>{
-    
-    
+    const searchalbum=search(`kkk,album`)
+    expect(fetchstub).to.been.calledWith(`https://api.spotify.com/v1/me`)
 }
 it(`should exist the search method`),()=>{
    
  
 }}
+}
 )
 
 }
