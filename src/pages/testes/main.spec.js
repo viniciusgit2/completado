d952f53ed8e1d2af579becdb80a645d85e2c46d4
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-18 11:16:37 -0300

    refatorado e com mais dados

--- src/pages/testes/main.spec.js
+++ src/pages/testes/main.spec.js
@@ -1,26 +1,31 @@
 import {expect, should,} from "chai"
-import { search ,searchplaylist,searchalbum,seartrack } from "../spote/main"
+import { search ,searchplaylist,searchalbum,searchtrack, searchartist } from "../spote/main"
 import sinonChai from "sinon-chai"
 import sinonStubPromise from "sinon-stub-promise"
 import Sinon from "sinon"
 import fetch from "node-fetch"
 import { global } from "styled-jsx/css"
+import { resolve } from "path"
 
 
 const fetchedstub =sinon.stub(global,`fetch`)
+fetchedstub.returnsPromise();
 const artist=search()
 describe( `spote`),()=>{
    describe( `smoke-test`),()=>{
    }
-
+}
 
 
 describe( `generic search`),()=>{
     let fetchedstub;
+    let promise;
+
     beforeEach(()=>{fetchedstub=Sinon.stub(global,`fetch`)});
+    promise=fetchedstub.returnsPromise();
     afterEach(()=>{fetchedstub.restore();
     })
-
+}
 it( `should call fetched function`),()=>{
 
 
@@ -55,10 +60,36 @@ context(`passing more types`),()=>{
     fetchedstub.restore();
 
 }
+it (`should the json data from the promise`,()=>{
+promise.resolve(`body:json`);
+const artist=search(`kkk`,`artist`)
+expect(artist.resolve).to.be.eql(`body:json`)
+}
+
+)
+context(`passing more types`),()=>{
+    it (`should call fetch function`,()=>{
+    const artist=searchartist(`kkk`)
+    expect(fetchedstub().to.have.been.calledWith(`	https://api.spotify.com/v1/me=artist`)
+    )
+    context(`search album`),()=>{
+        const album=searchalbum(`kkkk`)
+        expect(fetchedstub)
+    }
+    context(`search track`),()=>{
+        const track=searchtrack(`kkkk`)
+        expect(fetchedstub)
+    }
+    context(`search playlist`),()=>{
+        const playlist=searchplaylist(`kkkk`)
+        expect(fetchedstub)
+}
+    }
+    )
 
 }
 
-}
+
 
 
 
