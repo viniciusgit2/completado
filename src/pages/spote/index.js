72d373f2a9eaf3d7a2594281480f18a00a4872ef
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-20 10:18:58 -0300

    copia incompleta

---
+++ src/pages/spote/index.js
@@ -0,0 +1,7 @@
+import { search,searchartist,searchplaylist } from "./main";
+import { getalbum,getalbumtracks,getalbums } from "./album";
+module.exports={
+    search,searchartist,searchplaylist,
+    getalbum,getalbumtracks,getalbums
+
+}
